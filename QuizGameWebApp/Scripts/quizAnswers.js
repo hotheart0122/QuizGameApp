b3e0be67477364d94f45fa4b2d488de2190e4d1d
Kevin Hwangi KIM <Kevin Hwangi KIM>
2017-03-07 15:58:48 -0500

    initial commit

---
+++ QuizGameWebApp/Scripts/quizAnswers.js
@@ -0,0 +1,39 @@
+﻿$(document).ready(function () {
+    var addAnswerButton = document.getElementById("add-answer-button"),
+        answerDiv = document.getElementById("answer-div"),
+        nextId = 0;
+
+    var panelTemplate = '<div class="form-group">' +
+                          '<label class="control-label col-md-3" for="Answers[{{index}}].Id"> Id </label>' +
+                          '<input type="text" class="form-control" name="Answers[{{index}}].Id" id="Answers[{{index}}].Id" value="{{index}}" />' +
+                        '</div>' +
+
+                        '<div class="form-group">' +
+                          '<label class="control-label col-md-3" for="Answers[{{index}}].Content">Content </label>' +
+                          '<input type="text" class="form-control" name="Answers[{{index}}].Content" id="Answers[{{index}}].Content" />' +
+                        '</div>' +
+
+                        '<div class="form-group">' +
+                          '<label class="control-label col-md-3" for="Answers[{{index}}].IsCorrect">Is Correct </label>' +
+                          '<input type="checkbox" name="Answers[{{index}}].IsCorrect" id="Answers[{{index}}].IsCorrect" value="true" />' +
+                          '<input type="hidden" name="Answers[{{index}}].IsCorrect" id="Answers[{{index}}].IsCorrect" value="false" />' +
+                        '</div>';
+
+    addAnswerButton.addEventListener("click", function () {
+        var newAnswerHtml = panelTemplate.replace(/{{index}}/g, nextId++),
+            newAnswerDiv = document.createElement("div");
+        newAnswerDiv.classList.add("panel");
+        newAnswerDiv.innerHTML = newAnswerHtml;
+        answerDiv.appendChild(newAnswerDiv);
+    });
+
+});
+
+
+
+
+
+
+
+
+
---
+++ QuizLibrary/QuizAnswer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizLibrary
+{
+    public class QuizAnswer
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public bool IsCorrect { get; set; }
+        [Required]
+        public string Content { get; set; }
+        public string ImageUrl { get; set; }
+        public string Reason { get; set; }
+
+        public override string ToString()
+        {
+            return Id + " " + Content;
+        }
+    }
+}
